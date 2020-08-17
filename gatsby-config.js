898cf1072506dee5bd83e976eedbe9f878f57193
tekjoe <ramirez.josephh@gmail.com>
2020-08-17 11:48:01 -0500

    fixed issue with gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gastby-plugin-sitemap`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
